da129f9d619fd5186990a1ec0ac7a3fbffa91311
jdungan <johnadungan@gmail.com>
2020-06-10 19:30:52 -0500

    revive notebooks add a bunch of local work

--- scripts/get_cases.py
+++ scripts/get_cases.py
@@ -1,22 +1,21 @@
 # script for downloading Cases
 import pickle
+import zlib
 
 import oscn
 
-years = ['2017']
-counties = ['tulsa']
-types =['CM','CF']
-
-
+years = ['2017', '2018']
+counties = ['mayes', 'delaware', 'tulsa']
+types = ['CM', 'CF']
 
 for county in counties:
-  for year in years:
-    for type in types:
-      cases=oscn.request.CaseList(type=type, year=year, county=county)
-      # all_cases= ['howdy']
-      all_cases = [c for c in cases]
-      file_name = f'./data/{county}.{year}.{type}.oscn'
-      f = open(file_name, 'wb')
-      pickle.dump(all_cases, f)
-      f.close()
-      print(f'{file_name}: {len(all_cases)}')
+    for year in years:
+        for type in types:
+            cases = oscn.request.CaseList(type=type, year=year, county=county)
+            all_cases = [c for c in cases]
+            file_name = f'./data/{county}.{year}.{type}.cases'
+            f = open(file_name, 'wb')
+            compressed_cases = zlib.compress(pickle.dumps(all_cases), 6)
+            f.write(compressed_cases)
+            f.close()
+            print(f'{file_name}: {len(all_cases)}')
